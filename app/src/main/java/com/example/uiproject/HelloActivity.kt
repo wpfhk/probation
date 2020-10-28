741f818bcca78bc2697f9d31d67cfdabebefece5
Everspin <youngsang.kwon@Everspinui-Macmini.local>
2020-10-28 17:04:42 +0900

    Initial commit

---
+++ app/src/main/java/com/example/uiproject/HelloActivity.kt
@@ -0,0 +1,12 @@
+package com.example.uiproject
+
+import android.app.Activity
+import android.os.Bundle
+
+class HelloActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
